713c2b75fc0f8dfe97cf36342d6e28182f93fd31
Kramskoy Vyacheslav <kramskoy.va@gmail.com>
2025-07-13 13:08:23 +0300

    feat: правка контента и стилей

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -5,20 +5,22 @@ const swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   spaceBetween: 0.5,
   centeredSlides: true,
-  mousewheel: true,
   effect: "coverflow",
   pagination: {
     el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
   breakpoints: {
     769: {
       slidesPerView: 3,
       spaceBetween: 50,
       direction: "vertical",
-      pagination: {
-        enabled: false,
-      },
     },
+
     576: {
       slidesPerView: 1.7,
       spaceBetween: 10,
